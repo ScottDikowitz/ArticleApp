0bd56d603726f2070f436f7e4073c72bc209368c
Scott Dikowitz <dikowitz@gmail.com>
2015-12-22 16:53:56 -0500

    Create XHR and article loop.

--- articles.js.jsx
+++ articles.js.jsx
@@ -1,10 +1,35 @@
 var Articles = React.createClass({
 
   getInitialState: function(){
-    return ({ });
+    return ({articles: [] });
+  },
+
+  getJson: function(){
+
+    var that = this;
+    var request = new XMLHttpRequest();
+    request.open('GET', './data/articles.json', true);
+    request.onload = function() {
+      console.log("1234");
+        if (request.status >= 200 && request.status < 400) {
+        var my_JSON_object = JSON.parse(request.responseText);
+        console.log(my_JSON_object);
+        that.setState({articles: my_JSON_object});
+      }
+    };
+    request.send();
+  },
+
+  componentDidMount: function(){
+    this.getJson();
   },
 
   render: function(){
-    return <div></div>;
+    return <div>
+      {this.state.articles.map(function(el){
+        return <li key={el.id}>{el.title}</li>;
+
+      })}
+    </div>;
   }
 });
