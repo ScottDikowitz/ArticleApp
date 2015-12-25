05fc5be1599de46c6993cced6385e1c71fc70d48
Scott Dikowitz <dikowitz@gmail.com>
2015-12-25 12:38:52 -0500

    Create header component.

--- article_line.js.jsx
+++ article_line.js.jsx
@@ -2,6 +2,8 @@ var ArticleLine = React.createClass({
 
   render: function(){
 
-    return <li className='article-line'>{this.props.article.title}</li>;
+    return <li className='article-line'>
+      <a href={this.props.article.url}>{this.props.article.title}</a>
+    </li>;
   }
 });
---
+++ header.js.jsx
@@ -0,0 +1,17 @@
+var Header = React.createClass({
+
+
+render: function(){
+  return <div>
+    <header class="header group">
+        <ul class="header-list">
+          <li><a href="#">Author</a></li>
+          <li><a href="#">Words</a></li>
+          <li><a href="#">Submitted</a></li>
+        </ul>
+    </header>
+  <Articles/>
+  </div>;
+}
+
+});
