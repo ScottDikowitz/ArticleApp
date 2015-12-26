c8a26c99119e1d321688ef878c44e4137b5859e7
Scott Dikowitz <dikowitz@gmail.com>
2015-12-26 18:02:07 -0500

    Article grid formatting.

--- app/assets/javascripts/components/article_line.js.jsx
+++ app/assets/javascripts/components/article_line.js.jsx
@@ -22,10 +22,11 @@ var ArticleLine = React.createClass({
   render: function(){
 
     return <li className='article-line group'>
-      <div className="title">
-      <a href={this.props.article.url}>{this.props.article.title}</a>
-      </div>
+
       <div className="details">
+        <div className="title">
+        <a href={this.props.article.url}>{this.props.article.title}</a>
+        </div>
         <div className="author">
           <a href="#">{this.props.article.profile.first_name + " " + this.props.article.profile.last_name}</a>
       </div>
--- app/assets/javascripts/components/header.js.jsx
+++ app/assets/javascripts/components/header.js.jsx
@@ -15,7 +15,7 @@ timeSort: function(){
 render: function(){
   return <div>
     <header className="header group">
-        <ul className="header-list">
+        <ul className="header-list group">
           <li><a href="#">Unpublished Articles</a></li>
           <li><a onClick={this.authorSort} href="#">Author</a></li>
           <li><a onClick={this.wordSort} href="#">Words</a></li>
