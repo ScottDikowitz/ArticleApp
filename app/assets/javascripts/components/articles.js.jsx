f208bbb5398db4c98cd3e05bf3c2f08f974e2f2a
Scott Dikowitz <dikowitz@gmail.com>
2015-12-26 15:18:39 -0500

    Create change handlers for article store.

--- app/assets/javascripts/components/articles.js.jsx
+++ app/assets/javascripts/components/articles.js.jsx
@@ -4,25 +4,19 @@ var Articles = React.createClass({
     return ({articles: [] });
   },
 
-  getJson: function(){
+  _changed: function(){
 
-    var that = this;
-    var request = new XMLHttpRequest();
-    request.open('GET', './assets/data/articles.json', true);
-    request.onload = function() {
-      console.log("1234");
-        if (request.status >= 200 && request.status < 400) {
-        var my_JSON_object = JSON.parse(request.responseText);
-        console.log(my_JSON_object);
-        that.setState({articles: my_JSON_object});
-      }
-    };
-    request.send();
+    this.setState({articles: ArticleStore.all()});
   },
 
   componentDidMount: function(){
-    this.getJson();
-    // ApiUtil.fetchArticles();
+    // this.getJson();
+    ArticleStore.addChangeHandler(this._changed);
+    ApiUtil.fetchArticles();
+  },
+
+  componentWillUnmount: function(){
+    ArticleStore.removeChangeHandler(this._changed);
   },
 
   render: function(){
--- app/assets/javascripts/util/api_util.js
+++ app/assets/javascripts/util/api_util.js
@@ -5,11 +5,9 @@ var ApiUtil = window.ApiUtil = {
     var request = new XMLHttpRequest();
     request.open('GET', './assets/data/articles.json', true);
     request.onload = function() {
-      console.log("1234");
         if (request.status >= 200 && request.status < 400) {
-        var my_JSON_object = JSON.parse(request.responseText);
-        // console.log(my_JSON_object);
-        debugger;
+        var articles = JSON.parse(request.responseText);
+        ApiActions.receiveAll(articles);
 
       }
     };
